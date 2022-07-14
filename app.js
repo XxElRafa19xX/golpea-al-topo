f6bb7711bb85aec9f2e41358ea9f4d8f6d51c2cf
Rafael Alcantar <105615422+XxElRafa19xX@users.noreply.github.com>
2022-07-14 18:47:45 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+const squares = document.querySelectorAll('.square');
+const mole = document.querySelector('.mole');
+const score = document.querySelector('#score');
+const timeLeft = document.querySelector('#time-left');
+
+let result = 0;
+let hitPosition;
+let currentTime = 30;
+let timerId = null;
+
+function RandomSquare() {
+    squares.forEach(square => {
+        square.classList.remove('mole');
+    });
+
+    let randomSquare = squares[Math.floor(Math.random()* 6 )];
+    randomSquare.classList.add('mole');
+
+    hitPosition = randomSquare.id;
+}
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if(square.id == hitPosition) {
+            result++;
+            score.textContent = result;
+            hitPosition = null;
+        }
+    });
+})
+
+function moveMole() {
+    timerId = setInterval(RandomSquare, 500);
+}
+
+
+
+function countDown() {
+    currentTime--;
+    timeLeft.textContent = currentTime;
+
+    if(currentTime == 0) {
+        clearInterval(timerId);
+        clearInterval(countDownTimerId);
+        document.getElementById('btnIniciar').style.display = "block";
+        alert("¡FIN DEL JUEGO! tu puntuación: " + result);
+    }
+}
+
+let countDownTimerId;
+
+function BeginGame() {
+    countDownTimerId = setInterval(countDown, 1000);
+    moveMole();
+    document.getElementById('btnIniciar').style.display = "none";
+    {
+        document.getElementById('audio').src ="Fall Guys Background Music Pa Pari Pa.mp3";
+    }
+}
